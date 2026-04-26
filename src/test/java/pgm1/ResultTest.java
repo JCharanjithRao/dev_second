3ca0563e5c62d025b1777de8ce39a671001f106b
J Charanjith Rao <jadhavcharan05@gmail.com>
2026-04-26 12:28:27 +0530

    reset

--- src/test/java/pgm1/ResultTest.java
+++ src/test/java/pgm1/ResultTest.java
@@ -4,19 +4,19 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ResultTest {
-	
-	@Test
-	public void testpass() {
-		Assert.assertEquals(Result.display(55),"pass");
-	}
-	
-	@Test
-	public void testfail() {
-		Assert.assertEquals(Result.display(55),"fail");
-	}
-	@Test
-	public void testinvalid() {
-		Assert.assertEquals(Result.display(55),"invalid");
-	}
-	
+
+    @Test
+    public void testPass() {
+        Assert.assertEquals(Result.display(55), "pass");
+    }
+
+    @Test
+    public void testFail() {
+        Assert.assertEquals(Result.display(35), "fail");
+    }
+
+    @Test
+    public void testInvalid() {
+        Assert.assertEquals(Result.display(-5), "invalid");
+    }
 }
