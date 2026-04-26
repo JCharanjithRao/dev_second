caf26416624ba6767c8cb6eb101fa476257a8335
J Charanjith Rao <jadhavcharan05@gmail.com>
2026-04-26 12:20:32 +0530

    I am retard

--- src/main/java/pgm1/Result.java
+++ src/main/java/pgm1/Result.java
@@ -3,7 +3,7 @@ package pgm1;
 public class Result {
 	public static String display(int m) {
 		if(m<0||m>100) 
-			return"Invalid";
+			return"invalid";
 			else if(m<40)
 				return"fail";
 			else
